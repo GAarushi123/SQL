c3c36b01e46a99edbf7dbb47876ee72baf15ea5e
Atishay Jain <atishayjain2003@gmail.com>
2023-06-06 00:15:20 +0530

    DELETION FROM TABLE

---
+++ DELETION.sql
@@ -0,0 +1,30 @@
+-- Create the new cats table:
+
+CREATE TABLE cats (
+    cat_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+); 
+-- Insert some cats:
+
+INSERT INTO cats(name, breed, age) 
+VALUES ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
+       
+SELECT * FROM CATS
+
+-- Delete all 4 year old cats:
+
+DELETE FROM cats WHERE age=4;
+-- Delete cats where cat_id is the same as their age:
+
+DELETE FROM cats WHERE cat_id=age;
+
+

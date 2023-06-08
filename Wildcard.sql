57d00625513b02ebb912721918d3a4db52d5bade
Atishay Jain <atishayjain2003@gmail.com>
2023-06-08 22:49:04 +0530

    Finding Names that start with J

--- Wildcard.sql
+++ Wildcard.sql
@@ -4,3 +4,5 @@ select * from books;
 select author_fname from books where author_fname like "%ave";
 -- Find names of authors that ends with A
 select distinct author_fname from books where author_fname like "%a";
+-- Find first name of author that end with J
+select author_fname from books where author_fname like "J%";
